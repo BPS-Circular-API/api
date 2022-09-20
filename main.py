ceef8edb0bf8341dfa77914c1e81caaca67a7e77
Raj Dave <10864@bpsdoha.edu.qa>
2022-09-20 19:12:16 +0300

    rewrite frontend api responses

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ ptm = cat_dict["ptm"]
 general = cat_dict["general"]
 exam = cat_dict["exam"]
 
-class CatAndRecInput(BaseModel):
+class CategoryInput(BaseModel):
     category: str
-    receive: str = "all"
-
 
 class TitleInput(BaseModel):
     title: str
@@ -20,6 +18,18 @@ class UrlInput(BaseModel):
 
 app = FastAPI()
 
+success_response = {
+    "status": "success",
+    "http_status": 200,
+    "data": []
+}
+
+error_response = {
+    "status": "error",
+    "http_status": 400,
+    "data": []
+}
+
 
 @app.get("/")
 async def root():
@@ -28,46 +38,72 @@ async def root():
 
 # Get RAW circular lists
 @app.get("/list/")
-async def _get_circular_list(userinput: CatAndRecInput):
-    category, receive = userinput.category.lower(), userinput.receive.lower()
+async def _get_circular_list(userinput: CategoryInput):
+    category = userinput.category.lower()
 
     url = ptm if category == "ptm" else general if category == "general" else exam if category == "exam" else None
     if url is None:
         return HTTPException(status_code=400, detail="Category not found")
-    if not receive.lower() in ["all", "titles", "links"]:
-        return HTTPException(status_code=400, detail="Receive not found")
 
-    return get_circular_list(url, receive.lower())
+    res = get_circular_list(url)
+
+    return_list = success_response
+    print(res)
+
+    for element in res:
+        print(element)
+        title = element['title']
+        link = element['link']
+
+        return_list['data'].append({"title": title, "url": link})
+
+    return return_list
 
 
 # Get latest circular
 @app.get("/latest/")
-async def _get_latest_circular(userinput: CatAndRecInput):
-    category, receive = userinput.category.lower(), userinput.receive.lower()
+async def _get_latest_circular(userinput: CategoryInput):
+    category =  userinput.category.lower()
 
     url = ptm if category == "ptm" else general if category == "general" else exam if category == "exam" else None
     if url is None:
         return HTTPException(status_code=400, detail="Category not found")
-    if not receive.lower() in ["all", "titles", "links"]:
-        return HTTPException(status_code=400, detail="Receive not found")
 
-    return get_latest_circular(url, receive.lower())
+
+    res = get_latest_circular(url)
+
+    return_list = {
+        "status": "success",
+        "http_status": 200,
+        "data": []
+    }
+
+    return_list['data'].append(res)
+    return return_list
 
 
 @app.get("/search/")
 async def _search(userinput: TitleInput):
     title = userinput.title
 
-    all_titles = get_circular_list(pages_list, "titles")
+    all_titles = get_circular_list(page_list)
     res = get_most_similar_sentence(title, all_titles)
     return get_download_url(res)
 
 
 @app.get("/cached-latest/")
-async def _get_cached_latest_circular(userinput: CatAndRecInput):
-    x = get_cached_latest_circular(userinput.category.lower(), userinput.receive.lower())
-    return x
+async def _get_cached_latest_circular(userinput: CategoryInput):
+    res = get_cached_latest_circular(userinput.category.lower())
+    return_list = success_response
+
+    return_list['data'].append(res)
+    return return_list
 
 @app.get("/getpng")
 async def _get_png(urlinput: UrlInput):
-    return get_png(urlinput.url)
+    res = get_png(urlinput.url)
+
+    return_list = success_response
+    return_list['data'].append(res)
+
+    return return_list
