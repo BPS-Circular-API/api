b4171dbdc3d32740e914e51bc35f83dbfae09d57
Raj Dave <10864@bpsdoha.edu.qa>
2022-09-26 17:41:54 +0300

    backend for non-hardcoded URLs

--- backend.py
+++ backend.py
@@ -1,43 +1,22 @@
 import bs4, requests, threading, pickle, time, os
 import pypdfium2 as pdfium
 
-cat_dict = {
-    "ptm": (
-        "https://www.bpsdoha.net/circular/category/40", # PTM (Page 1)
-        "https://www.bpsdoha.net/circular/category/40?start=20", # PTM (Page 2)
-        "https://www.bpsdoha.net/circular/category/40?start=40", # PTM (Page 3)
-        ),
+default_pages = 5
 
-    "general": (
-        "https://www.bpsdoha.net/circular/category/38",  # General (Page 1)
-        "https://www.bpsdoha.net/circular/category/38?start=20",  # General (Page 2)
-        "https://www.bpsdoha.net/circular/category/38?start=40"  # General (Page 3)
-        "https://www.bpsdoha.net/circular/category/38?start=60"  # General (Page 4)
-    ),
+def page_generator(category: str or int, pages: int = default_pages) -> tuple:
+    preset_cats = {"general": 38, "ptm": 40, "exam": 35}
+    if type(category) == str:
+        category = preset_cats[category]
 
-    "exam": (
-        "https://www.bpsdoha.net/circular/category/35",  # Exam (Page 1)
-        "https://www.bpsdoha.net/circular/category/35?start=20"  # Exam (Page 2)
-        "https://www.bpsdoha.net/circular/category/35?start=40"  # Exam (Page 3)
-    )
-}
+    urls = []
+    # generate urls incrementing by 20 but starting from 0
+    for i in range(0, pages * 20, 20):
+        urls.append(f"https://www.bpsdoha.net/circular/category/{category}?start={i}")
 
-page_list = (
-    "https://www.bpsdoha.net/circular/category/40",  # PTM (Page 1)
-    "https://www.bpsdoha.net/circular/category/40?start=20",  # PTM (Page 2)
-    "https://www.bpsdoha.net/circular/category/40?start=40",  # PTM (Page 3)
-
-    "https://www.bpsdoha.net/circular/category/38",  # General (Page 1)
-    "https://www.bpsdoha.net/circular/category/38?start=20",  # General (Page 2)
-    "https://www.bpsdoha.net/circular/category/38?start=40"  # General (Page 3)
-    "https://www.bpsdoha.net/circular/category/38?start=60"  # General (Page 4)
-
-    "https://www.bpsdoha.net/circular/category/35",  # Exam (Page 1)
-    "https://www.bpsdoha.net/circular/category/35?start=20"  # Exam (Page 2)
-    "https://www.bpsdoha.net/circular/category/35?start=40"  # Exam (Page 3)
-)
+    return tuple(urls)
 
 
+page_list = tuple([page_generator("general"), page_generator("ptm"), page_generator("exam")])
 
 
 def per_url(url, old_titles, unprocessed_links, roll) -> None:
@@ -53,11 +32,11 @@ def per_url(url, old_titles, unprocessed_links, roll) -> None:
 
 def get_circular_list(url: tuple):
     titles, links, unprocessed_links, threads, old_titles = [], [], [], [], []
+
     for URL in range(len(url)):
         old_titles.append([])
         unprocessed_links.append([])
-        threads.append(threading.Thread(target=lambda: per_url(
-            url[URL], old_titles, unprocessed_links, URL)))
+        threads.append(threading.Thread(target=lambda: per_url(url[URL], old_titles, unprocessed_links, URL)))
         threads[-1].start()
     for thread in threads:
         thread.join()
@@ -69,6 +48,7 @@ def get_circular_list(url: tuple):
         titles += old_title
     circulars = [{"title": title, "link": link} for title, link in zip(titles, links)]
     return circulars
+
 
 
 def get_latest_circular(category: tuple):
@@ -91,7 +71,7 @@ def thread_function_for_get_download_url(title, url, mutable):
             mutable.append(str(titles_soup[title_no].text).strip())
 
 
-def get_download_url(title: str):
+def get_download_url(title: str) -> tuple or None:
     mutable, threads = [], []
 
     for URL in page_list:
@@ -107,15 +87,11 @@ def get_download_url(title: str):
 
 
 def store_latest_circular():
-    ptm = cat_dict["ptm"]
-    general = cat_dict["general"]
-    exam = cat_dict["exam"]
-
     while True:
         data = {
-            "ptm": get_latest_circular(ptm),
-            "general": get_latest_circular(general),
-            "exam": get_latest_circular(exam)
+            "ptm": get_latest_circular(page_generator('ptm')),
+            "general": get_latest_circular(page_generator('general')),
+            "exam": get_latest_circular(page_generator('exam'))
         }
         with open("temp.pickle", "wb") as f:
             pickle.dump(data, f)
@@ -159,6 +135,8 @@ def get_png(download_url) -> str:
     os.remove(f"./{file_id}.pdf")
 
     return f"https://bpsapi.rajtech.me/circularpng/{file_id}.png"
+
+
 
 
 # this is a daemon thread, daemon process get auto-terminated when the program ends, so don't worry about the while loop
