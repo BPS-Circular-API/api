1c35aadd0f6c6ea6b5f4fa43b6592d8b5449d576
Raj Dave <10864@bpsdoha.edu.qa>
2024-04-13 19:50:51 +0300

    bump requirements

--- backend.py
+++ backend.py
@@ -53,23 +53,23 @@ class LogConfig(BaseModel):
     LOG_LEVEL: str = "INFO"
 
     # Logging config
-    version = 1
-    disable_existing_loggers = False
-    formatters = {
+    version: int = 1
+    disable_existing_loggers: bool = False
+    formatters: dict = {
         "default": {
             "()": "uvicorn.logging.DefaultFormatter",
             "fmt": LOG_FORMAT,
             "datefmt": "%Y-%m-%d %H:%M:%S",
         },
     }
-    handlers = {
+    handlers: dict = {
         "default": {
             "formatter": "default",
             "class": "logging.StreamHandler",
             "stream": "ext://sys.stderr",
         },
     }
-    loggers = {
+    loggers: dict = {
         "bps-circular-api": {"handlers": ["default"], "level": LOG_LEVEL},
     }
 
