cb7ed734d392fc52bec0fe17f6772cbb77026088
Raj Dave <rajdave8002@gmail.com>
2025-07-22 18:58:10 +0300

    Updates tests for circular and PNG retrieval
    Refactors tests for retrieving circular lists and PNGs by updating category IDs and URLs to reflect current data.
    
    Removes redundant test for category ID lookups.
    
    Adds a more specific test case for invalid circular IDs, ensuring correct error handling and data integrity. Also adds a test for when no data is returned.

--- tests/tests.py
+++ tests/tests.py
@@ -13,7 +13,7 @@ from main import (
     _new_circulars
 )
 from backend import categories
-
+import json
 
 class CircularList(unittest.TestCase):
     def test_category(self):
@@ -33,7 +33,7 @@ class CircularList(unittest.TestCase):
         self.assertEqual(type(val), starlette.responses.JSONResponse)
 
     def test_category_id(self):
-        val = asyncio.run(_get_circular_list(52))  # general, as of 8/4/24
+        val = asyncio.run(_get_circular_list(55))  # general, as of 8/4/24
 
         self.assertEqual(val["status"], "success")
         self.assertEqual(val["http_status"], 200)
@@ -70,17 +70,6 @@ class CircularLatest(unittest.TestCase):
         self.assertNotEqual(val['data']['link'], "")
         self.assertNotEqual(val['data']['id'], "")
 
-    def test_id_less_category(self):
-        val = asyncio.run(_get_latest_circular("primary"))
-
-        self.assertEqual(val["status"], "success")
-        self.assertEqual(val["http_status"], 200)
-        self.assertEqual(type(val), dict)
-
-        self.assertNotEqual(val['data']['title'], "")
-        self.assertNotEqual(val['data']['link'], "")
-        self.assertNotEqual(val['data']['id'], "")
-
     def test_invalid_category(self):
         val = asyncio.run(_get_latest_circular("invalid"))
 
@@ -89,7 +78,7 @@ class CircularLatest(unittest.TestCase):
         self.assertIn('Invalid category', val.body.decode())
 
     def test_category_id(self):
-        val = asyncio.run(_get_latest_circular(52))
+        val = asyncio.run(_get_latest_circular(55))
 
         self.assertEqual(val["status"], "success")
         self.assertEqual(val["http_status"], 200)
@@ -120,6 +109,7 @@ class CircularSearch(unittest.TestCase):
 
     def test_invalid_query(self):
         val = asyncio.run(_search("invalid_query"))
+        print(val)
 
         self.assertEqual(val["status"], "success")
         self.assertEqual(val["http_status"], 200)
@@ -182,7 +172,7 @@ class CircularPNG(unittest.TestCase):
 
     def test_invalid_id(self):
         val = asyncio.run(
-            _get_png("https://bpsdoha.com/circular/category/52-academic-year-2023-24?download=6969"))
+            _get_png("https://bpsdoha.com/circular/category/55-academic-year-2023-24?download=6969"))
 
         self.assertEqual(type(val), starlette.responses.JSONResponse)
         self.assertEqual(val.status_code, 400)
@@ -190,7 +180,7 @@ class CircularPNG(unittest.TestCase):
 
     def test_invalid_url(self):
         val = asyncio.run(
-            _get_png("https://notbpsdoha.com/circular/category/52-academic-year-2023-24?download=1337"))
+            _get_png("https://notbpsdoha.com/circular/category/55-academic-year-2023-24?download=1337"))
 
         self.assertEqual(type(val), starlette.responses.JSONResponse)
         self.assertEqual(val.status_code, 422)
@@ -200,7 +190,7 @@ class CircularPNG(unittest.TestCase):
 class NewCirculars(unittest.TestCase):
     def test_existing_circular_id(self):
         # Replace with an existing circular ID to test
-        circular_id = 1756
+        circular_id = 1969
         val = asyncio.run(_new_circulars(circular_id))
 
         # Assuming that the response is successful when a valid circular ID is provided
@@ -213,15 +203,33 @@ class NewCirculars(unittest.TestCase):
         # Ensure that no circular in the list matches the provided circular_id
         self.assertNotIn(circular_id, [circular['id'] for circular in val['data']])
 
-    def test_nonexistent_circular_id(self):
-        # Test with an unlikely to exist circular ID (e.g., 99999)
-        circular_id = 99999
+    def test_invalid_circular_id(self):
+        # Replace with a circular ID that doesn't exist but the id is between 2 real circular ids
+        circular_id = 1913
         val = asyncio.run(_new_circulars(circular_id))
+        print(val)
+
+        # Assuming that the response is successful when a valid circular ID is provided
+        self.assertEqual(val["status"], "success")
+        self.assertEqual(val["http_status"], 200)
+        self.assertEqual(type(val), dict)
+
+        # Check that the data returned is a list of circulars
+        self.assertEqual(type(val["data"]), list)
+        # Ensure that no circular in the list matches the provided circular_id
+        self.assertNotIn(circular_id, [circular['id'] for circular in val['data']])
+        # Ensure that no circular id in the list has id <circular_id
+        self.assertNotEqual(0, len([int(circular['id']) < circular_id for circular in val['data']]))
+
+
+    def test_9999_circular_id(self):
+        # Test with an unlikely to exist circular ID (e.g., 9999)
+        circular_id = 9999
+        val = asyncio.run(_new_circulars(circular_id))
+        print(val)
 
         # Since this ID is invalid, expect an error response
-        self.assertEqual(type(val), starlette.responses.JSONResponse)
-        self.assertEqual(val.status_code, 422)
-        self.assertIn('Circular ID does not exist', val.body.decode())
+        self.assertEqual(len(val['data']), 0)
 
 
 
@@ -231,7 +239,6 @@ class AdditionalTests(unittest.TestCase):
 
         self.assertEqual(val["status"], "success")
         self.assertEqual(val["http_status"], 200)
-        self.assertEqual(type(val), dict)
         self.assertEqual(val['data'], [i for i in categories.keys()])
 
     def test_root(self):
